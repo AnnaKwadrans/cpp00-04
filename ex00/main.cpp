553d1558a03141683fa6c9f5285751da1bf32e27
Anna Kwadrans <akwadran@c3r14s6.42madrid.com>
2026-03-13 21:36:21 +0100

    fixed point

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include "Fixed.hpp"
 
@@ -14,4 +15,26 @@ int main( void )
     std::cout << c.getRawBits() << std::endl;
     
     return 0;
+}
+*/
+
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+
+int main (void)
+{
+    double pi = 3.141592653589;
+
+    std::cout << std::setprecision(15) << pi << std::endl;
+
+    double op1 = cos(pi/2);
+
+    std::cout << std::setprecision(15) << op1 << std::endl;
+
+    double op2 = op1 / 1e-17;
+
+    std::cout << std::setprecision(15) << op2 << std::endl;
+    
+    return (0);
 }
